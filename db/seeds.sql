4b6d1380f3982f6fc443dcc2512fbe88cb3de14f
Tomas Vasquez <baylor@Tomass-MacBook-Pro.local>
2018-02-21 07:53:45 -0600

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Whataburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whataburger Jr');
